9d57df25c735f646c8bf210f2df452144bf66e1d
= <350765427@qq.com>
2015-08-16 10:46:47 +0800

    init repo and commit

---
+++ CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by jiechen on 15/8/16.
+//  Copyright © 2015年 jch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreDataDemo
+//
+//  Created by jiechen on 15/8/16.
+//  Copyright © 2015年 jch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
